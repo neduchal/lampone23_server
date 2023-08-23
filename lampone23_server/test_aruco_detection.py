a615c995f0dfdfeba34b1e7b12505fff0683a142
Petr Neduchal <neduchal@gmail.com>
2023-08-23 11:32:42 +0200

    Test nižšího rozlišení (fullHD)

--- lampone23_server/test_aruco_detection.py
+++ lampone23_server/test_aruco_detection.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2
 import cv2.aruco
 
-cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, width=(int)3264, height=(int)2464, format=(string)BGRx ! videoconvert ! appsink')
+cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! appsink')
+#cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, width=(int)3264, height=(int)2464, format=(string)BGRx ! videoconvert ! appsink')
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
