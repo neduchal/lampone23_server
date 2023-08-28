b8aec15dbdc55c08b928219b82702fa37fa3b901
Petr Neduchal <neduchal@gmail.com>
2023-08-28 07:12:40 +0200

    Predelani timeru na ukladani obrazku

--- lampone23_server/image_grabber.py
+++ lampone23_server/image_grabber.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import Image
 from std_msgs.msg import Empty, String # For trigger message
 import numpy as np
+import time
 #import scipy
 #import skimage
 #from skimage import transform as tf
@@ -18,10 +19,11 @@ class ImageGrabber(Node):
     def __init__(self):
         super().__init__('image_grabber')
         self.image_publisher = self.create_publisher(Image, "/image", 10)
-        self.timer = self.create_timer(5, self.image_saver_callback)
+        #self.timer = self.create_timer(5, self.image_saver_callback)
         self.cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! appsink')
         self.image = None
         self.bridge = CvBridge()
+        self.current_time = time.time()
 
         if not self.cap.isOpened():
             print("Cannot open camera")
@@ -31,11 +33,15 @@ class ImageGrabber(Node):
         while True:
             print("OK")
             ret, frame = self.cap.read()
- 
+
             # if frame is read correctly ret is True
             if not ret:
                 print("Can't receive frame (stream end?). Exiting ...")
                 break
+            if time.time() - self.current_time > 5:
+                cv2.imwrite("/var/www/html/image/image.png", frame)
+                self.current_time = time.time()
+
             self.image = frame
             self.image_publisher.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
