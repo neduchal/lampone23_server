c99002990f2d106cfc53683ad66ebb9e4ae39f7d
Petr Neduchal <neduchal@gmail.com>
2023-08-28 07:15:07 +0200

    Odstraneni nepotrebnych casti image_grabberu

--- lampone23_server/image_grabber.py
+++ lampone23_server/image_grabber.py
@@ -19,7 +19,6 @@ class ImageGrabber(Node):
     def __init__(self):
         super().__init__('image_grabber')
         self.image_publisher = self.create_publisher(Image, "/image", 10)
-        #self.timer = self.create_timer(5, self.image_saver_callback)
         self.cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! appsink')
         self.image = None
         self.bridge = CvBridge()
@@ -31,7 +30,6 @@ class ImageGrabber(Node):
 
     def run(self):
         while True:
-            print("OK")
             ret, frame = self.cap.read()
 
             # if frame is read correctly ret is True
@@ -44,10 +42,6 @@ class ImageGrabber(Node):
 
             self.image = frame
             self.image_publisher.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
-
-    def image_saver_callback(self):
-        if self.image is not None:
-            cv2.imwrite("/var/www/html/image/image.png", self.image)
 
 def main(args=None):
     rclpy.init(args=args)
