2bf92e18d0ff1aaac0bd47f2ec0d221aa6cfe134
Petr Neduchal <neduchal@gmail.com>
2023-08-29 11:12:26 +0200

    Pridani ulozeni normalizovaneho obr

--- lampone23_server/test_playground_detection.py
+++ lampone23_server/test_playground_detection.py
@@ -37,6 +37,7 @@ def nms(input, neighborhood_size=40):
 def get_grid(img):
     # Normalize image
     normalized = normalize_img(img)
+    cv2.imwrite("/var/www/html/image/norm.jpg", normalized)
 
     # Creating kernel with pattern
     kernel = np.ones((9,9))
