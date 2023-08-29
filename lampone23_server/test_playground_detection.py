a2e629b7dd1a12ab5826a9d8c92feab574000012
Petr Neduchal <neduchal@gmail.com>
2023-08-29 08:54:19 +0200

    zmena v testu detekce hriste

--- lampone23_server/test_playground_detection.py
+++ lampone23_server/test_playground_detection.py
@@ -1,22 +1,11 @@
 import cv2
 import numpy as np
 
-cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! appsink')
-#cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, width=(int)3264, height=(int)2464, format=(string)BGRx ! videoconvert ! appsink')
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+
+frame = cv2.imread("/var/www/html/image/image.png")
 
 
-while True:
+image = frame[260:761, 652:1328, :]
 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+cv2.imwrite("/var/www/html/image/test.jpg", image)
 
-    image = frame[260:761, 652:1328, :]
-
-    cv2.imwrite("/var/www/html/image/test.jpg", image)
-
-    # orez
-
-    break
