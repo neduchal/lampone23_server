d334002626839f4f870492d576cb5a5cf85d6cec
Petr Neduchal <neduchal@gmail.com>
2023-08-29 08:42:03 +0200

    Vztvoreni launch filu, test detekce hriste

---
+++ lampone23_server/test_playground_detection.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! appsink')
+#cap =  cv2.VideoCapture(f'nvarguscamerasrc sensor-mode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv ! video/x-raw, width=(int)3264, height=(int)2464, format=(string)BGRx ! videoconvert ! appsink')
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    image = frame[260:761, 652:1328, :]
+
+    cv2.imwrite("/var/www/html/image/test.png")
+
+    # orez
--- launch/lampone23_server_launch.py
+++ launch/lampone23_server_launch.py
@@ -12,38 +12,24 @@ from launch.substitutions import PathJoinSubstitution, TextSubstitution
 
 def generate_launch_description():
 
-    realsense_launch_dir = os.path.join(
-        get_package_share_directory('realsense2_camera'), 'launch')
-
-    realsense_launch = IncludeLaunchDescription(
-        launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(realsense_launch_dir, 'rs_launch.py')),
-        #launch_arguments={'camera_name':'home_navigation'}.items()
-    )
-
-    scan_height_arg = DeclareLaunchArgument(
-        'scan_height', default_value=TextSubstitution(text='100')
-    )
-    range_max_arg = DeclareLaunchArgument(
-        'range_max', default_value=TextSubstitution(text='3')
-    )    
 
     return LaunchDescription([
-        realsense_launch,
-        scan_height_arg,
-        range_max_arg,
         Node(
-            package='depthimage_to_laserscan',
-            namespace='home_navigation',
-            executable='depthimage_to_laserscan_node',
-            name='sim',
-            remappings=[
-                ('/home_navigation/depth', '/camera/depth/image_rect_raw'),
-                ('/home_navigation/depth_camera_info', '/camera/depth/camera_info'),
-            ],
-            parameters=[
-                {'scan_height': 50},
-                {'range_max': 5.0}
-            ]
-        )
+            package='lampone23_server',
+            namespace='lampone23',
+            executable='image_grabber',
+            name='lampone23_image_grabber'
+        ),
+       Node(
+            package='lampone23_server',
+            namespace='lampone23',
+            executable='server',
+            name='lampone23_server'
+        ),
+       Node(
+            package='lampone23_server',
+            namespace='lampone23',
+            executable='controller',
+            name='lampone23_controller'
+        )        
     ])
