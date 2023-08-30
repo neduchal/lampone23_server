438629ac52155b88afc149c9f953d0abcac08cf7
Petr Neduchal <neduchal@gmail.com>
2023-08-30 09:45:18 +0200

    Pridani whilu

--- lampone23_server/controller.py
+++ lampone23_server/controller.py
@@ -211,11 +211,12 @@ class LamponeServerRobotController(Node):
 
 
     def run(self):
-        if time.time() > self.current_time + 1:
-            if len(self.path) > 0  and self.image is not None and self.trigger == True:
-                self.process_path(self.path.pop(0))
-                self.trigger = None
-            self.current_time = time.time()
+        while True:
+            if time.time() > self.current_time + 1:
+                if len(self.path) > 0  and self.image is not None and self.trigger == True:
+                    self.process_path(self.path.pop(0))
+                    self.trigger = None
+                self.current_time = time.time()
         
     def trigger_callback(self, msg):
         self.trigger = True
