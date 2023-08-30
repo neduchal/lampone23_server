57cbaae54df91eedd352fe3ff69287e55caaf6b0
Petr Neduchal <neduchal@gmail.com>
2023-08-30 11:49:17 +0200

    zASTAVENI A ZMENA RYCHLOSTI

--- lampone23_server/controller.py
+++ lampone23_server/controller.py
@@ -184,7 +184,7 @@ class LamponeServerRobotController(Node):
                 elif current_move == "R":
                     move_msg.angular.z = 1.0
                 elif current_move == "F":
-                    move_msg.linear.x = 0.75
+                    move_msg.linear.x = 1
                     last_angle = last_state[2]
                     current_angle = current_state[2]
                     diff = 0
@@ -201,7 +201,7 @@ class LamponeServerRobotController(Node):
                     elif diff < -3:
                         move_msg.angular.z = -0.1
                 elif current_move == "B":
-                    move_msg.linear.x = -0.75
+                    move_msg.linear.x = -1
                     if last_angle > 350 or last_angle < 10:
                         pass
                     elif last_angle > 80 and last_angle < 100:
@@ -220,8 +220,8 @@ class LamponeServerRobotController(Node):
                 #print(move_msg)
                 self.twist_publisher.publish(move_msg)
             # Poslani zpravy na zastaveni
-        move_msg = Twist()
-        self.twist_publisher.publish(move_msg)                
+            move_msg = Twist()
+            self.twist_publisher.publish(move_msg)                
 
 
     def run_callback(self):
